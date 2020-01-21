a3f37b8302acd955f5652afb37b2bd6c6d04d309
Kisuk Jeong Account <jeongks@byui.edu>
2020-01-21 19:52:27 +0900

    edit to mine

--- JS.js
+++ JS.js
@@ -1,4 +1,18 @@
 let today = new Date();
-let date = today.getDate()+'/'+today.getMonth()+'/'+today.getFullYear()+' '+today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
+let date = today.getDate() + '/' + String(today.getMonth() + 1).padStart(2, '0') + '/' + today
+    .getFullYear() + ' ' + today.getHours() + ':' + String(today.getMinutes()).padStart(2, '0') + ':' + String(
+        today.getSeconds()).padStart(2, '0');
 
-document.getElementById("currentdate").textContent = date;
+current = document.getElementById("date");
+
+function getFontSize(x) {
+    if (x.matches) {
+        current.style.fontSize = "80%";
+    } else {
+        current.style.fontSize = "100%";
+    }
+}
+
+var x = window.matchMedia("(max-wdith: 34.99rem)")
+getFontSize(x)
+x.addListener(getFontSize)
