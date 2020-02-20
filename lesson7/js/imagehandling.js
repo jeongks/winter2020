2205fbc376df7fe599940f45de95c098030513c8
Kisuk Jeong Account <jeongks@byui.edu>
2020-02-20 11:14:40 +0900

    image loader

---
+++ lesson7/js/imagehandling.js
@@ -0,0 +1,65 @@
+const images = documet.querySelectAll("[data-src]");
+
+function preloadImage(img){
+    const src = img.getAttribute("data-src");
+    if (!src){
+        return;
+    }
+
+    img.src = src;
+}
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        } else{
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+
+iamges.forEach(image => {
+    imgObserver.observe(image);
+});
+
+// const loadImages = (image) => {
+//     image.setAttribute('src', image.getAttribute('data-src'));
+//     image.onload = () => {image.removeAttribute('data-src');};
+// };
+
+// function preloadImage(img){
+//     const src = img.getAttribute("data-src");
+//     if (!src){
+//         return;
+//     } 
+//     img.src = src;
+// }
+
+// if('IntersectionObserver' in window) {
+//     const imgObserver = new IntersectionObserver((items, imgObserver) => {
+//         items.forEach((item) => {
+//             if(items.isIntersecting){
+//                 loadImages(item.target);
+//                 imgObserver.unobserve(item.target);
+//             }
+
+//         });
+//     }, imgOptions);
+
+//     imagesLoader.forEach((img) => {
+//         imgObserver.observe(img);
+//     });
+// }
+// else {
+//     imagesLoader.forEach((img) =>{
+//         loadImages(img);
+//     });
+// }
