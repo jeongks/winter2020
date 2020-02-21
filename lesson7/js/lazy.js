a237d9157a811a4b8bdf552e1384735df0889e19
Kisuk Jeong Account <jeongks@byui.edu>
2020-02-22 07:24:49 +0900

    change lesson 7

--- lesson7/js/lazy.js
+++ lesson7/js/lazy.js
@@ -1,6 +1,6 @@
-const images = documet.querySelectAll("[data-src]");
+const images = document.querySelectorAll("[data-src]");
 
-function preloadImage(img){
+function loadImages(img){
     const src = img.getAttribute("data-src");
     img.onload = () => {
         img.removeAttribute('data-src');
@@ -13,50 +13,19 @@ function preloadImage(img){
 }
 
 const imgOptions = {
-    threshold: 1,
+    threshold: 0,
     rootMargin: "0px 0px 10px 0px"
 };
-
-// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-//     entries.forEach(entry => {
-//         if (!entry.isIntersecting){
-//             return;
-//         } else{
-//             preloadImage(entry.target);
-//             imgObserver.unobserve(entry.target);
-//         }
-//     })
-// }, imgOptions);
-
-
-// iamges.forEach(image => {
-//     imgObserver.observe(image);
-// });
-
-// const imagesLoader = document.querySelectorAll("[data-src");
-
-// const loadImages = (image) => {
-//     image.setAttribute('src', image.getAttribute("data-src"));
-//     image.onload = () => {image.removeAttribute("data-src");};
-// };
-
-// function preloadImage(img){
-//     const src = img.getAttribute("data-src");
-//     if (!src){
-//         return;
-//     } 
-//     img.src = src;
-// }
 
 if('IntersectionObserver' in window) {
     const imgObserver = new IntersectionObserver((items, imgObserver) => {
         items.forEach((item) => {
-            if(items.isIntersecting){
+            if(!items.isIntersecting){
                 loadImages(item.target);
                 imgObserver.unobserve(item.target);
             }
         });
-    });
+    }, imgOptions);
 
     images.forEach((img) => {
         imgObserver.observe(img);
@@ -64,6 +33,6 @@ if('IntersectionObserver' in window) {
 }
 else {
     images.forEach((img) =>{
-        loadImages(img);
+        imgObserver.observe(img);
     });
 }
