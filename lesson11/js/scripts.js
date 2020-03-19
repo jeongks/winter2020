7d7cd8f435acd7d14db87387e722644eafb869c3
jeongks <kisuk.jeong.lds@gmail.com>
2020-03-19 18:04:45 +0900

    lessson 11

--- lesson11/js/scripts.js
+++ lesson11/js/scripts.js
@@ -41,7 +41,7 @@ fetch(apiURL)
         forcastDayNumber += 1;
         if ( forcastDayNumber === 7){forcastDayNumber = 0;}
 
-        let dayName = document.createElement("span");
+        let dayName = document.createElement("h5");
         dayName.textContent = myweekday[forcastDayNumber];
 
         let theTemp = document.createElement("p");
@@ -61,34 +61,7 @@ fetch(apiURL)
         
       } 
       
-      let currentWeather = document.createElement("h4");
-        currentWeather.textContent = "Weather: " + weatherInfo.list[i].weather[0].main;
-  
-        let currentTemp = document.createElement("h4");
-        currentTemp.textContent = "Temperature: " + weatherInfo.list[i].main.temp;
-        
-        let windchill = document.createElement("h4");
-        windchill.textContent = "Windchill: " +  Math.round(35.74 + (weatherInfo.list[i].main.temp * 0.6215) 
-                                                            - (35.75 * Math.pow(weatherInfo.list[i].wind.speed,0.16)) 
-                                                            + (0.4275 * weatherInfo.list[i].main.temp * Math.pow(weatherInfo.list[i].wind.speed,0.16)) )
-                                + "\xB0";
-        let humidity = document.createElement("h4");
-        humidity.textContent = "Humidity: " + weatherInfo.list[i].main.humidity + "\u0025";
-  
-        let windSpeed = document.createElement("h4");
-        windSpeed.textContent = "Wind Speed: " + weatherInfo.list[i].wind.speed;
-  
-        let textBlock = document.createElement("div");
-  
-        textBlock.appendChild(currentWeather);
-        textBlock.appendChild(currentTemp);
-        textBlock.appendChild(windchill);
-        textBlock.appendChild(humidity);
-        textBlock.appendChild(humidity);
-        textBlock.appendChild(windSpeed);
-  
-        document.getElementById("text-block").appendChild(textBlock);
-
+      
     }
 
     
---
+++ lesson11/js/weather.js
@@ -0,0 +1,37 @@
+const forecastURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=f3dbc42cf2a8636fdc9d4f58d6627a4b&units=imperial";
+
+fetch(forecastURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        let currentWeather = document.createElement("p");
+        currentWeather.textContent = "Weather: " + jsObject.weather[0].main;
+  
+        let currentTemp = document.createElement("p");
+        currentTemp.textContent = "Temperature: " + jsObject.main.temp + "\xB0";
+        
+        let windchill = document.createElement("p");
+        windchill.textContent = "Windchill: " +  Math.round(35.74 + (jsObject.main.temp * 0.6215) 
+                                                            - (35.75 * Math.pow(jsObject.wind.speed,0.16)) 
+                                                            + (0.4275 * jsObject.main.temp * Math.pow(jsObject.wind.speed,0.16)) )
+                                + "\xB0";
+        let humidity = document.createElement("p");
+        humidity.textContent = "Humidity: " + jsObject.main.humidity + "\u0025";
+  
+        let windSpeed = document.createElement("p");
+        windSpeed.textContent = "Wind Speed: " + jsObject.wind.speed;
+  
+        let textBlock = document.createElement("div");
+  
+        textBlock.appendChild(currentWeather);
+        textBlock.appendChild(currentTemp);
+        textBlock.appendChild(windchill);
+        textBlock.appendChild(humidity);
+        textBlock.appendChild(humidity);
+        textBlock.appendChild(windSpeed);
+  
+        document.getElementById("text-block").appendChild(textBlock);
+
+
+    });
